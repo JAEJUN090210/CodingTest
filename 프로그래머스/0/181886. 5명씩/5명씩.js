b81c032354bd6a82fd39851cc84ddd6b1461097b
JAE JUN JEON <ssokage090210@naver.com>
2025-05-27 17:23:29 +0900

    [level 0] Title: 5명씩, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.js
+++ 프로그래머스/0/181886. 5명씩/5명씩.js
@@ -1,1 +1,9 @@
-function solution(names) {return [names[0], names[5], names[10], names[15], names[20], names[25],names[30]].filter(Boolean);}
+// function solution(names) {return [names[0], names[5], names[10], names[15], names[20], names[25],names[30]].filter(Boolean);}
+
+function solution(names) {
+    let answer = [];
+    for (let i = 0; i < names.length; i += 5) {
+        answer.push(names[i]);
+    }
+    return answer;
+}
