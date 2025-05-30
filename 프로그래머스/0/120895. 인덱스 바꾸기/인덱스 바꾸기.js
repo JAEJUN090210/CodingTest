c831fb242cb1491c6c2041f8e7efe580c57fee39
JAE JUN JEON <ssokage090210@naver.com>
2025-05-30 19:50:00 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,7 +1,10 @@
-function solution(my_string, num1, num2) {
-    let a = my_string.split('');
-    let temp = a[num1];
-    a[num1] = a[num2];
-    a[num2] = temp;
-    return a.join('');
+// function solution(my_string, num1, num2) {
+//     let a = my_string.split('');
+//     let temp = a[num1];
+//     a[num1] = a[num2];
+//     a[num2] = temp;
+//     return a.join('');
+// }
+
+const solution=(s,i,j)=>{var a=[...s];return a[i]=s[j],a[j]=s[i],a.join('')
 }
