34d8e30efea9d6068fcf7d79f24004526eceb845
JAE JUN JEON <ssokage090210@naver.com>
2025-06-13 19:40:03 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 46.66 ms, Memory: 32.2 MB -BaekjoonHub

--- 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
+++ 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
@@ -1,3 +1,43 @@
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// let input = [];
+
+// rl.on('line', function (line) {
+//     input.push(line);
+//     rl.close();
+// }).on('close', function () {
+//     const n = parseInt(input[0]);
+//     let result = '';
+
+//     if (n === 1) {
+//         result += '*\n';
+//     } else if (n === 2) {
+//         result += '*\n**\n';
+//     } else if (n === 3) {
+//         result += '*\n**\n***\n';
+//     } else if (n === 4) {
+//         result += '*\n**\n***\n****\n';
+//     } else if (n === 5) {
+//         result += '*\n**\n***\n****\n*****\n';
+//     } else if (n === 6) {
+//         result += '*\n**\n***\n****\n*****\n******\n';
+//     } else if (n === 7) {
+//         result += '*\n**\n***\n****\n*****\n******\n*******\n';
+//     } else if (n === 8) {
+//         result += '*\n**\n***\n****\n*****\n******\n*******\n********\n';
+//     } else if (n === 9) {
+//         result += '*\n**\n***\n****\n*****\n******\n*******\n********\n*********\n';
+//     } else if (n === 10) {
+//         result += '*\n**\n***\n****\n*****\n******\n*******\n********\n*********\n**********\n';
+//     }
+
+//     console.log(result);
+// });
+
 const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
@@ -8,31 +48,13 @@ let input = [];
 
 rl.on('line', function (line) {
     input.push(line);
-    rl.close();
+    rl.close(); // 입력이 한 줄이므로 바로 종료
 }).on('close', function () {
-    const n = parseInt(input[0]);
+    const n = parseInt(input[0]); // 입력값을 정수로 변환
     let result = '';
 
-    if (n === 1) {
-        result += '*\n';
-    } else if (n === 2) {
-        result += '*\n**\n';
-    } else if (n === 3) {
-        result += '*\n**\n***\n';
-    } else if (n === 4) {
-        result += '*\n**\n***\n****\n';
-    } else if (n === 5) {
-        result += '*\n**\n***\n****\n*****\n';
-    } else if (n === 6) {
-        result += '*\n**\n***\n****\n*****\n******\n';
-    } else if (n === 7) {
-        result += '*\n**\n***\n****\n*****\n******\n*******\n';
-    } else if (n === 8) {
-        result += '*\n**\n***\n****\n*****\n******\n*******\n********\n';
-    } else if (n === 9) {
-        result += '*\n**\n***\n****\n*****\n******\n*******\n********\n*********\n';
-    } else if (n === 10) {
-        result += '*\n**\n***\n****\n*****\n******\n*******\n********\n*********\n**********\n';
+    for (let i = 1; i <= n; i++) {
+        result += '*'.repeat(i) + '\n';
     }
 
     console.log(result);
