c67f16e4dd472fc7472c0bc2f58e0417d39e95f5
JAE JUN JEON <ssokage090210@naver.com>
2025-06-20 19:10:41 +0900

    [level 0] Title: 문자열의 뒤의 n글자, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.js
+++ 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.js
@@ -1,3 +1,5 @@
-function solution(my_string, n) {
-    return my_string.slice(-n);
-}
+// function solution(my_string, n) {
+//     return my_string.slice(-n);
+// }
+
+const solution = (my_string, n) => my_string.slice(-n)
