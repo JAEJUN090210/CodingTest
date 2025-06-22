3979bc5ef6b9392d83bd924337a3d9bda027708a
JAE JUN JEON <ssokage090210@naver.com>
2025-06-22 22:31:16 +0900

    [level 0] Title: 주사위의 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.js
@@ -1,7 +1,11 @@
-function solution(box, n) {
-  const widthCount = Math.floor(box[0] / n);
-  const lengthCount = Math.floor(box[1] / n);
-  const heightCount = Math.floor(box[2] / n);
+// function solution(box, n) {
+//   const widthCount = Math.floor(box[0] / n);
+//   const lengthCount = Math.floor(box[1] / n);
+//   const heightCount = Math.floor(box[2] / n);
   
-  return widthCount * lengthCount * heightCount;
+//   return widthCount * lengthCount * heightCount;
+// }
+
+function solution(box, n) {
+  return Math.floor(box[0] / n) * Math.floor(box[1] / n) * Math.floor(box[2] / n);
 }
