0d2928b8f9adb2788fcc6fd6b6c38e078c2e512f
JAE JUN JEON <ssokage090210@naver.com>
2025-06-23 20:03:05 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -0,0 +1,1 @@
+const solution = (numbers) => (numbers = numbers.sort((a, b) => a - b), ((numbers[0] * numbers[1]) >= (numbers[numbers.length-1] * numbers[numbers.length-2])) ? (numbers[0] * numbers[1]) : (numbers[numbers.length-1] * numbers[numbers.length-2]))
