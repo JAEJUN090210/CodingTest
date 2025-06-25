05599b0a4050e5732feffe907f70b4cd58c4fc46
JAE JUN JEON <ssokage090210@naver.com>
2025-06-25 23:14:02 +0900

    [level 0] Title: 가까운 1 찾기, Time: 2.20 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -1,9 +1,11 @@
-function solution(arr, idx) {
-    for(let i = idx; i <= arr.length; i ++){
-        if(arr[i] == 1){
-            return i;
-            break;
-        }
-    }
-    return -1;
-}
+// function solution(arr, idx) {
+//     for(let i = idx; i <= arr.length; i ++){
+//         if(arr[i] == 1){
+//             return i;
+//             break;
+//         }
+//     }
+//     return -1;
+// }
+
+const solution = (arr, idx) => arr.map((v, i) => i < idx ? 0 : v).indexOf(1)
