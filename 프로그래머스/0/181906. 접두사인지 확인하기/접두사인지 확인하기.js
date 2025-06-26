958256a71b8ac46da0d07e0b10604fbf184e9a9f
JAE JUN JEON <ssokage090210@naver.com>
2025-06-26 13:47:51 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -1,10 +1,12 @@
-function solution(my_string, is_prefix) {
-    my_string = my_string.split('');
-    is_prefix = is_prefix.split('');
-    for(let i = 0; i < is_prefix.length; i ++) {
-        if (my_string[i] !== is_prefix[i]){
-            return 0;
-        }
-    }
-    return 1;
-}
+// function solution(my_string, is_prefix) {
+//     my_string = my_string.split('');
+//     is_prefix = is_prefix.split('');
+//     for(let i = 0; i < is_prefix.length; i ++) {
+//         if (my_string[i] !== is_prefix[i]){
+//             return 0;
+//         }
+//     }
+//     return 1;
+// }
+
+const solution = (my_string, is_prefix) => my_string.startsWith(is_prefix) ? 1 : 0;
