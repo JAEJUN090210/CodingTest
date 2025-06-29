0e3cd60eaf8f94ccf5125ee7db74c90fba5167fe
JAE JUN JEON <ssokage090210@naver.com>
2025-06-29 21:03:05 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,7 +1,9 @@
-function solution(phone_number) {
-    phone_number = phone_number.split('');
-    for(let i = 0; i < phone_number.length - 4; i ++){
-        phone_number[i] = '*';
-    }
-    return phone_number.join('');
-}
+// function solution(phone_number) {
+//     phone_number = phone_number.split('');
+//     for(let i = 0; i < phone_number.length - 4; i ++){
+//         phone_number[i] = '*';
+//     }
+//     return phone_number.join('');
+// }
+
+const solution = phone_number => phone_number.split('').fill('*',0,phone_number.length-4).join('')
