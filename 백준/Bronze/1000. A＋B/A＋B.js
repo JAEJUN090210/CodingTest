f1312e42709dcbf1d9e82921ed77c6d0232973ca
JAE JUN JEON <ssokage090210@naver.com>
2025-07-02 14:29:38 +0900

    [Bronze V] Title: A+B, Time: 112 ms, Memory: 9488 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -0,0 +1,14 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let input = [];
+
+readline.on('line', function(line) {
+    input = line.split(' ').map(el => parseInt(el));
+}).on('close', function(){
+    const a = input[0];
+    const b = input[1];
+    console.log(a+b);
+});
