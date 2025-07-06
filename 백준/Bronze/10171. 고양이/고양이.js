bbe0eac8f7aaa3a4ee92c09f0865eeea19e7fe43
JAE JUN JEON <ssokage090210@naver.com>
2025-07-06 15:48:25 +0900

    [Bronze V] Title: 고양이, Time: 108 ms, Memory: 9456 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.js
@@ -0,0 +1,11 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+readline.on('close', function(){
+    console.log(`\\    /\\`);
+    console.log(` )  ( ')`);
+    console.log(`(  /  )`);
+    console.log(` \\(__)|`);
+});
