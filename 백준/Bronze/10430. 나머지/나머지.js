69ea62bf1779c1cfde7be06c26ba1967e2f72de8
JAE JUN JEON <ssokage090210@naver.com>
2025-07-07 15:40:54 +0900

    [Bronze V] Title: 나머지, Time: 112 ms, Memory: 9740 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', function(line) {
+  const [A, B, C] = line.split(' ').map(Number);
+
+  console.log((A + B) % C);
+  console.log(((A % C) + (B % C)) % C);
+  console.log((A * B) % C);
+  console.log(((A % C) * (B % C)) % C);
+});
