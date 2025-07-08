c0851f5017c33b131911a81caaa336147c1eee99
JAE JUN JEON <ssokage090210@naver.com>
2025-07-08 09:46:37 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 120 ms, Memory: 9740 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.js
@@ -0,0 +1,13 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let input = [];
+
+readline.on('line', function(line) {
+    input = line.split(' ').map(el => parseInt(el));
+}).on('close', function(){
+    const year = input[0];
+    console.log(year-543);
+});
