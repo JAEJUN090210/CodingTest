3cd0f2ffcc5db74d9c3234b9817fdbbcb6f73b75
JAEJUN JEON <ssokage090210@naver.com>
2025-07-17 20:27:13 +0900

    [Bronze V] Title: A-B, Time: 116 ms, Memory: 9484 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.js
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -1,14 +1,12 @@
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
+const readline = require('readline');
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
 });
 
-let input = [];
-
-readline.on('line', function(line) {
-    input = line.split(' ').map(el => parseInt(el));
-}).on('close', function(){
-    const a = input[0];
-    const b = input[1];
-    console.log(a-b);
+rl.on('line', (line) => {
+	const [num1, num2] = line.split(' ').map(Number); // 공백으로 나누고 숫자로 변환
+	console.log(num1 - num2); // 차이를 출력
+	rl.close();
 });
