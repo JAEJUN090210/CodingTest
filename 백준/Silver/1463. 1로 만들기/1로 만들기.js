587be3291b333f2f412719d11536831b8bb43798
JAEJUN JEON <ssokage090210@naver.com>
2025-07-27 22:09:14 +0900

    [Silver III] Title: 1로 만들기, Time: 184 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -0,0 +1,26 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on('line', function (line) {
+  const N = Number(line);
+  const dp = new Array(N + 1).fill(0); // dp[0]은 안 씀
+
+  for (let i = 2; i <= N; i++) {
+    dp[i] = dp[i - 1] + 1;
+
+    if (i % 2 === 0) {
+      dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+    }
+
+    if (i % 3 === 0) {
+      dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+    }
+  }
+
+  console.log(dp[N]);
+  rl.close();
+});
