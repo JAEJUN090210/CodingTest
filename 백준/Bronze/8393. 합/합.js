969e9da0b94739797b53ab0afbbc00ccf2a04104
JAEJUN JEON <ssokage090210@naver.com>
2025-08-04 20:31:51 +0900

    [Bronze V] Title: 합, Time: 112 ms, Memory: 9480 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.js
@@ -0,0 +1,18 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", function (line) {
+  const n = parseInt(line);
+  let sum = 0;
+
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+
+  console.log(sum);
+  rl.close();
+});
