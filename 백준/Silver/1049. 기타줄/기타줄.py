423f983872acca28003b5b60ea5f08c847a35e09
JAEJUN JEON <ssokage090210@naver.com>
2025-08-21 22:41:57 +0900

    [Silver IV] Title: 기타줄, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1049. 기타줄/기타줄.py
@@ -0,0 +1,17 @@
+N, M = map(int, input().split())
+
+min_package = float('inf')
+min_single = float('inf')
+
+for _ in range(M):
+    package, single = map(int, input().split())
+    min_package = min(min_package, package)
+    min_single = min(min_single, single)
+
+cost1 = ((N // 6) + 1) * min_package
+
+cost2 = N * min_single
+
+cost3 = (N // 6) * min_package + (N % 6) * min_single
+
+print(min(cost1, cost2, cost3))
