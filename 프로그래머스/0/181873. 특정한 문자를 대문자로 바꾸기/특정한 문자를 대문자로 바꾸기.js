2ec33dcb14e90e550e41eb518b5b608ad1482bf4
JAEJUN JEON <ssokage090210@naver.com>
2025-08-22 13:59:27 +0900

    [level 0] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
@@ -1,9 +1,11 @@
-function solution(my_string, alp) {
-    my_string = my_string.split("");
-    for(let i = 0; i < my_string.length; i++){
-        if(my_string[i] == alp){
-            my_string[i] = alp.toUpperCase();
-        }
-    }
-    return my_string.join("");
-}
+// function solution(my_string, alp) {
+//     my_string = my_string.split("");
+//     for(let i = 0; i < my_string.length; i++){
+//         if(my_string[i] == alp){
+//             my_string[i] = alp.toUpperCase();
+//         }
+//     }
+//     return my_string.join("");
+// }
+
+const solution = (my_string, alp) => my_string.replaceAll(alp, alp.toUpperCase())
