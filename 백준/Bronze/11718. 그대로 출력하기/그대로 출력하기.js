33e250281c50ce25716cea61126eb8edd9cb5423
JAEJUN JEON <ssokage090210@naver.com>
2025-10-27 15:16:55 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 116 ms, Memory: 9624 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.js
@@ -0,0 +1,14 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", (line) => {
+  input.push(line);
+}).on("close", () => {
+  input.forEach((line) => console.log(line));
+  process.exit();
+});
