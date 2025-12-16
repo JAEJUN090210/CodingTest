43f89e2078cc8fa68d322b9718f3812ea40e44a4
JAEJUN JEON <ssokage090210@naver.com>
2025-12-16 10:18:39 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,16 @@
+function solution(num_list) {
+    let evenSum = 0;
+    let oddSum = 0;
+    let sum = 0;
+    
+    for (let i = 0; i < num_list.length; i++) {
+        if (i % 2 === 0) {
+            evenSum += num_list[i];
+            sum += num_list[i];
+        } else {
+            oddSum += num_list[i];
+            sum -= num_list[i];
+        }
+    }
+    return (sum >= 0 ? evenSum : oddSum);
+}
