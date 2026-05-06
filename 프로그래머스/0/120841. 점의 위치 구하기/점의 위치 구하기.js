ba67e82ece7a6e419a6b5e1fa4ce99520c7d3e0c
JAEJUN JEON <ssokage090210@naver.com>
2026-05-06 22:02:27 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -1,17 +1,26 @@
+// function solution(dot) {
+//     let answer = 0;
+//     if(dot[0]>=0){
+//         if(dot[1]>=0){
+//            answer=1;
+//         }else{
+//             answer=4;
+//         }
+//     }else{
+//         if(dot[1]>=0){
+//            answer=2;
+//         }else{
+//             answer=3;
+//         }
+//     }
+//     return answer;
+// }
+
 function solution(dot) {
-    let answer = 0;
-    if(dot[0]>=0){
-        if(dot[1]>=0){
-           answer=1;
-        }else{
-            answer=4;
-        }
-    }else{
-        if(dot[1]>=0){
-           answer=2;
-        }else{
-            answer=3;
-        }
-    }
-    return answer;
+    const [x, y] = dot;
+
+    if (x >= 0 && y >= 0) return 1;
+    if (x < 0 && y >= 0) return 2;
+    if (x < 0 && y < 0) return 3;
+    return 4;
 }
