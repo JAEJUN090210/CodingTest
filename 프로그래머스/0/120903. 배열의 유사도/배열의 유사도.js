bbf7c179b4e919b1735fd5ca9694cf8e9aeb47e9
JAEJUN JEON <ssokage090210@naver.com>
2026-05-29 18:54:33 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 32.7 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,14 +1,13 @@
 function solution(s1, s2) {
     let answer = 0;
-    let s1length = s1.length;
-    let s2length = s2.length;
-    for(let i=0; i<=s1length; i++){
-        for(let n=0; n<=s2length; n++){
+
+    for(let i=0; i<s1.length; i++){
+        for(let n=0; n<s2.length; n++){
             if(s1[i] == s2[n]){
                 answer ++;
             }
         }
     }
-    answer = answer-1;
+
     return answer;
 }
